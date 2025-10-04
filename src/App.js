f9999b8091e3d16c2d53c27bae5f58d840afbc4e
Varsha Tembugade <varshatembugade346@gmail.com>
2025-10-04 18:43:10 +0530

    toastcontainer added in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import LandingPage from "./pages/LandingPage";
 import LoginSignup from "./pages/loginSignup";
 import Student_Dashboard from "./pages/StudentDashboard";
 import Warden_Dashboard from "./pages/Warden_Dashboard";
@@ -18,29 +21,25 @@ import AllocationHistory from "./pages/AllocationHistory";
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<LoginSignup />} />
         <Route path="/student-dashboard" element={<Student_Dashboard />} />
-        <Route path="/apply-leave" element={<Student_Leave/>} />
-        <Route path="/apply-complaint" element={<Student_Complaint/>} />
-        <Route path="/warden-dashboard" element={<Warden_Dashboard/>} />
-        <Route path="/warden/notices/create" element={<NoticeForm/>} />
-        <Route path="/warden/notices/update/:id" element={<UpdateNotice/>} />
-        <Route path="/warden/rooms" element={<RoomsPage/>} />
-        <Route path="/rooms/:roomNo" element={<RoomDetailPage/>} />
+        <Route path="/apply-leave" element={<Student_Leave />} />
+        <Route path="/apply-complaint" element={<Student_Complaint />} />
+        <Route path="/warden-dashboard" element={<Warden_Dashboard />} />
+        <Route path="/warden/notices/create" element={<NoticeForm />} />
+        <Route path="/warden/notices/update/:id" element={<UpdateNotice />} />
+        <Route path="/warden/rooms" element={<RoomsPage />} />
+        <Route path="/rooms/:roomNo" element={<RoomDetailPage />} />
         <Route path="/payment/:roomNo" element={<PaymentPage />} />
-        <Route path="/profile" element={<EditProfile/>} />
-        <Route path="/warden/allocations" element={<CurrentAllocations/>} />
-        <Route path="/warden/allocations/history" element={<AllocationHistory/>} />
-
+        <Route path="/profile" element={<EditProfile />} />
+        <Route path="/warden/allocations" element={<CurrentAllocations />} />
+        <Route path="/warden/allocations/history" element={<AllocationHistory />} />
       </Routes>
     </Router>
-    
   );
 }
 
 export default App;
-
-
-
