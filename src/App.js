1bf3c09a879a9771bd1270838d2eba851a2eaf46
Varsha Tembugade <varshatembugade346@gmail.com>
2025-10-04 18:45:10 +0530

    /auth/check route update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage"
 import LoginSignup from "./pages/loginSignup";
 import Student_Dashboard from "./pages/StudentDashboard";
 import Warden_Dashboard from "./pages/Warden_Dashboard";
@@ -17,29 +14,36 @@ import RoomDetailPage from "./pages/RoomDetails";
 import PaymentPage from "./pages/PaymentPage";
 import CurrentAllocations from "./pages/CurrentAllocations";
 import AllocationHistory from "./pages/AllocationHistory";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Router>
-      <ToastContainer />
+       <ToastContainer />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<LoginSignup />} />
         <Route path="/student-dashboard" element={<Student_Dashboard />} />
-        <Route path="/apply-leave" element={<Student_Leave />} />
-        <Route path="/apply-complaint" element={<Student_Complaint />} />
-        <Route path="/warden-dashboard" element={<Warden_Dashboard />} />
-        <Route path="/warden/notices/create" element={<NoticeForm />} />
-        <Route path="/warden/notices/update/:id" element={<UpdateNotice />} />
-        <Route path="/warden/rooms" element={<RoomsPage />} />
-        <Route path="/rooms/:roomNo" element={<RoomDetailPage />} />
+        <Route path="/apply-leave" element={<Student_Leave/>} />
+        <Route path="/apply-complaint" element={<Student_Complaint/>} />
+        <Route path="/warden-dashboard" element={<Warden_Dashboard/>} />
+        <Route path="/warden/notices/create" element={<NoticeForm/>} />
+        <Route path="/warden/notices/update/:id" element={<UpdateNotice/>} />
+        <Route path="/warden/rooms" element={<RoomsPage/>} />
+        <Route path="/rooms/:roomNo" element={<RoomDetailPage/>} />
         <Route path="/payment/:roomNo" element={<PaymentPage />} />
-        <Route path="/profile" element={<EditProfile />} />
-        <Route path="/warden/allocations" element={<CurrentAllocations />} />
-        <Route path="/warden/allocations/history" element={<AllocationHistory />} />
+        <Route path="/profile" element={<EditProfile/>} />
+        <Route path="/warden/allocations" element={<CurrentAllocations/>} />
+        <Route path="/warden/allocations/history" element={<AllocationHistory/>} />
+
       </Routes>
     </Router>
+    
   );
 }
 
 export default App;
+
+
+
